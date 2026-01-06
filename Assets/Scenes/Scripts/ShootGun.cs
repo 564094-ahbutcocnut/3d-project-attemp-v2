e51ad17b6e553d0acd97f8fc9470418e1ae2d605
564094-ahbutcocnut <564094@escg.ac.uk>
2026-01-06 21:54:21 +0000

    NM

---
+++ Assets/Scenes/Scripts/ShootGun.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ShootGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ExampleCoroutine");
+        // start the ExampleCoroutine()
+    }
+    if (Input.GetKeyDown(KeyCode.E))
+
+
+
+
+    // Speed at which the object will move 
+    public float moveSpeed = 5f;
+    public GameObject objectToSpawn;
+
+    private
+        void Awake()
+    {
+        transform.Rotate(180, 0, 0);
+    }
+    void Update()
+    {
+        ExampleCoroutine();
+    }
+
+
+
+    IEnumerator ExampleCoroutine()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+
+            Debug.Log("Move forward for 5 seconds");
+
+            // Move the object forward (in the object's local forward direction) 
+            float timer = 0f;
+            float moveduration = 5f;
+
+            while (timer < moveduration)
+            {
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+
+            Debug.Log("Wait for  1 seconds");
+            yield return new WaitForSeconds(1f);
+
+
+
+
+            // end of coroutine
+        }
+
+    }
+
+}
+
+if (Input.GetKeyDown(KeyCode.Space)) // Using GetKeyDown for cleaner single-press jump
+{
+   
+}
